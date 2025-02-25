da8ec5b2492dfaeea5d4b90c4e0da2fbac0d477c
bgcicca <landneco0@gmail.com>
2025-02-25 16:09:48 -0300

    refactoring

--- init.lisp
+++ init.lisp
@@ -8,7 +8,6 @@
 
 (lem-vi-mode:vi-mode)
 
-
 (add-hook *prompt-after-activate-hook*         
           (lambda ()           
             (call-command 'lem/prompt-window::prompt-completion nil)))(add-hook *prompt-deactivate-hook*   
@@ -24,6 +23,30 @@
 (setf lem/line-numbers:*relative-line* t)
 
 (setf lem:*auto-format* t)
+
+;;; repl 
+(define-key lem/listener-mode::*listener-mode-keymap* "C-c C-h" 'repl-history)
+
+(defparameter *history-size* 128)
+
+(defun buffer-history (buffer)
+  (alexandria-2:line-up-first
+   buffer
+   (lem/listener-mode::listener-history)
+   (lem/common/history:history-data-list)
+   (remove-duplicates :test #'equal)
+   (last *history-size*)
+   (reverse)))
+
+(define-command repl-history () ()
+  (lem/listener-mode:listener-clear-input)
+  (let* ((hist-completion (alexandria-2:rcurry #'completion
+                                               (buffer-history (current-buffer))
+                                               :test #'lem-core::fuzzy-match-p))
+         (expr (prompt-for-string "Expr: " :completion-function hist-completion)))
+    (insert-string (lem/listener-mode:input-start-point (current-buffer)) expr))
+  (move-to-end-of-buffer)
+  (lem/listener-mode:listener-return))
 
 ;; Keymaps
 (define-key lem-vi-mode:*normal-keymap* "Space s h" 'split-active-window-horizontally)
@@ -48,27 +71,3 @@
   ("C-h p" 'lem-lisp-mode:lisp-apropos-package)
   ("C-h f" 'lem-lisp-mode:lisp-describe-symbol))
 
-;;; repl 
-
-(define-key lem/listener-mode::*listener-mode-keymap* "C-c C-h" 'repl-history)
-
-(defparameter *history-size* 128)
-
-(defun buffer-history (buffer)
-  (alexandria-2:line-up-first
-   buffer
-   (lem/listener-mode::listener-history)
-   (lem/common/history:history-data-list)
-   (remove-duplicates :test #'equal)
-   (last *history-size*)
-   (reverse)))
-
-(define-command repl-history () ()
-  (lem/listener-mode:listener-clear-input)
-  (let* ((hist-completion (alexandria-2:rcurry #'completion
-                                               (buffer-history (current-buffer))
-                                               :test #'lem-core::fuzzy-match-p))
-         (expr (prompt-for-string "Expr: " :completion-function hist-completion)))
-    (insert-string (lem/listener-mode:input-start-point (current-buffer)) expr))
-  (move-to-end-of-buffer)
-  (lem/listener-mode:listener-return))
